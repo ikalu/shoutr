2ef3358f6fdad52b0a02a2848437c053eb02d4f5
Ifeanyi Kalu <ikalu13@gmail.com>
2014-07-14 16:38:43 -0400

    Add ability to shout
    * Add Shout model
    * Add shout form to dashboard#show
    * Add shouts#create
    * Extract shout partial

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,17 @@
+class ShoutsController < ApplicationController
+  def create
+    current_user.shouts.create(shout_params)
+
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def shout_params
+    params.
+      require(:shout).
+      permit(:body)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ require "monban/constraints/signed_in"
 
 Rails.application.routes.draw do
   constraints Monban::Constraints::SignedIn.new do
-    root "dashboards#show", as: :home
+    root "dashboards#show", as: :dashboard
   end
 
   root "homes#show"
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create]
   resource :dashboard, only: [:show]
 
 end
